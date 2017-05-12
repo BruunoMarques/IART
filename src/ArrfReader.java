898a59cb1493a26f2732dd7fe67635a58e6c7dbb
AtomicScale <up201303104@fe.up.pt>
2017-05-12 03:44:29 +0100

    Able to fetch elements from each of the dataset rows

--- src/ArrfReader.java
+++ src/ArrfReader.java
@@ -4,47 +4,70 @@ import java.io.IOException;
 import java.io.FileReader;
 import weka.core.Instances;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class ArrfReader {
 
     private String filePath;
     private Instances dataInstances;
-    private ArrayList<ArrayList<Double>> fullDataSet;
 
-    public ArrfReader(String filePath)
-    {
+    private ArrayList<ArrayList<Integer>> fullDataSet;
+
+    public ArrfReader(String filePath) {
         this.filePath = filePath;
         this.fullDataSet = new ArrayList<>();
 
         this.readFile();
-        //this.readDataSet();
+        this.readDataSet();
     }
 
+    public ArrayList<Integer> readPhishingData(int instanceNum) {
+        ArrayList<Integer> data = new ArrayList<>();
 
-    private void readFile(){
+        List<String> dataString = Arrays.asList(this.dataInstances.instance(instanceNum).toString().split(","));
 
-        try{
-        BufferedReader reader=new BufferedReader(new FileReader(this.filePath));
-        this.dataInstances=new Instances(reader);
-        reader.close();
+        for (int i = 0; i < dataString.size(); i++) {
+            String s = dataString.get(i);
 
-        // setting class attribute
-        dataInstances.setClassIndex(dataInstances.numAttributes()-1);
+                data.add(Integer.parseInt(s));
         }
-                catch(FileNotFoundException e)
-                {
-                    e.printStackTrace();
-                }
-                catch(IOException e)
-                {
-                    e.printStackTrace();
-                }
+
+        return data;
+    }
+
+    private void readFile() {
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(this.filePath));
+            this.dataInstances = new Instances(reader);
+            reader.close();
+
+            dataInstances.setClassIndex(dataInstances.numAttributes() - 1);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
     public static void main(String[] args) {
 
-        ArrfReader reader = new ArrfReader("");
+        ArrfReader reader = new ArrfReader("C:\\Users\\Vitor Esteves\\Documents\\IART\\data\\dataset.arff");
 
-        System.out.print(reader);
+        System.out.print(reader.getPhishingData(0).get(1));
+    }
+
+    public ArrayList<Integer> getPhishingData(int number){
+        ArrayList<Integer> ret = new ArrayList<>(this.fullDataSet.get(number));
+
+        return ret;
+    }
+
+    private void readDataSet() {
+        for (int i = 0; i < this.dataInstances.numInstances() - 1; i++) {
+            this.fullDataSet.add(readPhishingData(i));
+        }
     }
 }
